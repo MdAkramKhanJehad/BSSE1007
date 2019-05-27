b03b037f7d427b2a9637fc9042dfddbfc518bad8
MdAkramKhanJehad <46520536+MdAkramKhanJehad@users.noreply.github.com>
2019-05-27 19:42:23 +0600

    Add files via upload

--- GetAllPossibleIP.c
+++ GetAllPossibleIP.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include<stdlib.h>
-#include <string.h> 
+#include <string.h>
 #include <ifaddrs.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -15,7 +15,7 @@ unsigned int bitshift(char addr[]){
     unsigned int final=0;
     int n1, n2, n3, n4;
     sscanf(addr, "%d.%d.%d.%d", &n1, &n2, &n3, &n4);
-    
+
     n1 = n1<<24;
     n2 = n2<<16;
     n3 = n3<<8;
@@ -47,10 +47,8 @@ void convertIpAddress(unsigned int ip, unsigned char IPaddr[15]) {
    strcat(IPaddr,".");
    sprintf(temp,"%d",bytes[0]);
    strcat(IPaddr,temp);
-   
-  // printf("hahah = %s\n", IPaddr);
-   
-   
+
+
 }
 
 
@@ -66,55 +64,55 @@ void initializeIPArray(){
         if (ifa->ifa_addr->sa_family==AF_INET) {
             addr = inet_ntoa(((struct sockaddr_in *) ifa->ifa_addr)->sin_addr);
 
-            if(!(strcmp(ifa->ifa_name,"lo")==0)){  
+            if(!(strcmp(ifa->ifa_name,"lo")==0)){
                 break;
             }
         }
     }
-    freeifaddrs(ifap);           
+    freeifaddrs(ifap);
     strcpy(ip,addr);
-     
-    getifaddrs (&ifap);  
+
+    getifaddrs (&ifap);
     for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
         if (ifa->ifa_addr->sa_family==AF_INET) {
             addr2 = inet_ntoa(((struct sockaddr_in *) ifa->ifa_netmask)->sin_addr);
 
-            if(!(strcmp(ifa->ifa_name,"lo")==0)) 
+            if(!(strcmp(ifa->ifa_name,"lo")==0))
                 break;
-            
+
         }
     }
     freeifaddrs(ifap);
 
     strcpy(netmask,addr2);
-    
-    
+
+
     ipaddr = bitshift(ip);
     net = bitshift(netmask);
     int size = (~net)-1;
     unsigned char allPossibleIp[size][15], tempIP[15];
-    
+
     for(int i=1 ; i<(~net) ;i++){
         strcpy(tempIP, "\0");
         temp = (ipaddr & net) +i;
         convertIpAddress(temp,tempIP);
-        strcpy(allPossibleIp[i-1],tempIP); 
+        strcpy(allPossibleIp[i-1],tempIP);
 
     }
-
+    /*
     for(int i=1 ; i<(~net) ;i++){
-       
-        printf("%d = %s\n",i, allPossibleIp[i-1]); 
+
+        printf("%d = %s\n",i, allPossibleIp[i-1]);
 
     }
-
-
+    */
+    printf("source : %s\n",ip);
 }
 
 
 int main(void){
-    
+
     initializeIPArray();
-  
+
     return 0;
 }
